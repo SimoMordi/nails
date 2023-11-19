70f53746d4d9200cb34513d0f9aecec8b2d8f35f
simo M <simo.mordi@gmail.com>
2023-11-19 09:51:05 -0800

    add more photos

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -13,15 +13,35 @@ import img10 from '../../Assets/Images/img10.png'
 import img11 from '../../Assets/Images/img11.png'
 import img12 from '../../Assets/Images/img12.png'
 import img13 from '../../Assets/Images/img13.png'
-import img14 from '../../Assets/Images/img14.jpg'
+import img14 from '../../Assets/Images/img14.png'
+import img15 from '../../Assets/Images/img15.png'
+import img16 from '../../Assets/Images/img16.png'
+import img17 from '../../Assets/Images/img17.png'
+import img18 from '../../Assets/Images/img18.png'
+import img19 from '../../Assets/Images/img19.png'
+import img20 from '../../Assets/Images/img20.png'
+import img21 from '../../Assets/Images/img21.png'
+import img22 from '../../Assets/Images/img22.png'
+import img23 from '../../Assets/Images/img23.png'
+import img24 from '../../Assets/Images/img24.png'
+import img25 from '../../Assets/Images/img25.png'
+import img26 from '../../Assets/Images/img26.png'
+import img27 from '../../Assets/Images/img27.png'
+import img28 from '../../Assets/Images/img28.png'
+import img29 from '../../Assets/Images/img29.png'
+import img30 from '../../Assets/Images/img30.png'
+import img31 from '../../Assets/Images/img31.png'
+import img32 from '../../Assets/Images/img32.png'
 import logo from '../../Assets/Logo/logo.png'
 
 
 
 const imagePaths = [
     img1,img2,img3,img4,img5,img6,img7,
-    img8,img9,img10,img11,img12,img13,img14
- 
+    img8,img9,img10,img11,img12,img13,img14,
+    img15,img16,img17,img18,img19,
+    img20,img21,img22,img23,img24,img25,
+    img26,img27,img28,img29,img30,img31,img32
     
   ];
 
