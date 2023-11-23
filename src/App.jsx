23da143b36e11b4b89446a4381fd0648e35319cf
simo M <simo.mordi@gmail.com>
2023-11-22 23:10:20 -0800

    services page splits to multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,21 @@ import Gallery from './pages/Gallery/Gallery'
 import About from './pages/About/About'
 import Footer from './components/Footer/footer'
 import Navbar from './components/Navbar/Navbar'
+import ServicesSection from './components/mainServices/mainServices'
 
 function App() {
 
   return (
     <div>
 
-    <Navbar />
+      {/* <Navbar /> */}
       {/* <Contact/>  */}
-
-      <Gallery />
+     <ServicesSection />
+      {/* <Gallery /> */}
 
       {/* <About/> */}
-    <br />
-    <Footer />
+  
+      {/* <Footer /> */}
 
         {/* <Routes>
             <Route path='/' element={<Home/>}/>
---
+++ src/components/AdditionalServices/AdditionalServices.jsx
@@ -0,0 +1,18 @@
+// AdditionalServices.jsx
+import React from 'react';
+import './AdditionalServices.css';
+
+const AdditionalServices = () => {
+  return (
+    <section className="additional-services">
+      <article className="kids-services">
+        {/* Content for kids services */}
+      </article>
+      <article className="waxing-services">
+        {/* Content for waxing services */}
+      </article>
+    </section>
+  );
+};
+
+export default AdditionalServices;
