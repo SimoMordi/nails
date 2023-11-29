69d16669b02295feeee922b3aa1b784fecf6d200
unknown <akshatrait@gmail.com>
2023-11-29 00:52:38 -0800

    Aki's code

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Gallery from './pages/Gallery/Gallery'
 
 import ContactInfo from './components/ContactInfo/contactInfo'
 import Services from './pages/Services/Services'
+import Home from './pages/Home/Home'
 
 
 function App() {
 
   return (
     <div>
+      <Home/>
       {/* <Contact/>  */}
-      <Services />
+      {/* <Services /> */}
       {/* <Gallery /> */}
 
       
